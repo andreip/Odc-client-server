25db4c1560abe3775ebf0c6e828df03e87278a2a
Andrei Petre <p31andrei@gmail.com>
2014-04-17 18:22:25 +0300

    uiMediator.getUsername calls Mediator.getUsername(). #18

--- Odc-client/src/tests/TestUIMediator.java
+++ Odc-client/src/tests/TestUIMediator.java
@@ -14,12 +14,15 @@ import junit.framework.TestCase;
 
 public class TestUIMediator extends TestCase {
 	UIMediator uimed;
+	Mediator medMock;
 
 	public void setUp() {
 		Logger mockLogger = mock(Logger.class);
 		Whitebox.setInternalState(UIMediator.class, "logger", mockLogger);
 
+		medMock = mock(Mediator.class);
 		uimed = UIMediator.getInstance();
+		uimed.registerMediator(medMock);
 	}
 
 	public void testUIMediatorSingleton() {
@@ -31,5 +34,15 @@ public class TestUIMediator extends TestCase {
 		assertEquals(0, userListModel.size());
 		uimed.addUser("A");
 		assertEquals(1, userListModel.size());
+
+	}
+
+	/* Test that uiMediator does not know about usernames, and
+	 * in order to get them, it has to check with Mediator.
+	 */
+	public void testGetUsername() {
+		verify(medMock, times(0)).getUsername();
+		uimed.getUsername();
+		verify(medMock, times(1)).getUsername();
 	}
 }
