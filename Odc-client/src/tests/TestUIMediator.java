14356db789119336102b15ef59aab261000058fa
Andrei Petre <p31andrei@gmail.com>
2014-04-17 18:13:18 +0300

    Test that userListModel is populated on uiMediator.addUser. #18

--- Odc-client/src/tests/TestUIMediator.java
+++ Odc-client/src/tests/TestUIMediator.java
@@ -1,16 +1,35 @@
 package tests;
 
+import main.Mediator;
+import models.UserListModel;
+
+import org.apache.log4j.Logger;
+import org.powermock.reflect.Whitebox;
+
+import static org.mockito.Mockito.*;
+
 import gui.UIMediator;
+import gui.UserList;
 import junit.framework.TestCase;
 
 public class TestUIMediator extends TestCase {
 	UIMediator uimed;
 
 	public void setUp() {
+		Logger mockLogger = mock(Logger.class);
+		Whitebox.setInternalState(UIMediator.class, "logger", mockLogger);
+
 		uimed = UIMediator.getInstance();
 	}
 
 	public void testUIMediatorSingleton() {
 		assertEquals(uimed, UIMediator.getInstance());
+	}
+
+	public void testAddUserToModel() {
+		UserListModel userListModel = new UserListModel(uimed);
+		assertEquals(0, userListModel.size());
+		uimed.addUser("A");
+		assertEquals(1, userListModel.size());
 	}
 }
