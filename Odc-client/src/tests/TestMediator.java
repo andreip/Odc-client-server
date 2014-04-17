7053cab301e35b7f1d7642b0f18f467c6f8f19ab
Andrei Petre <p31andrei@gmail.com>
2014-04-17 22:10:53 +0300

    Assert Mediator calls UIMediator for rendering concerned logic. #18

--- Odc-client/src/tests/TestMediator.java
+++ Odc-client/src/tests/TestMediator.java
@@ -1,9 +1,12 @@
 package tests;
 
 import static org.mockito.Mockito.*;
-import gui.UIMediator;
 
 import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.SwingUtilities;
+
+import gui.UIMediator;
 
 import network.Network;
 import network.NetworkWorker;
@@ -17,7 +20,7 @@ import main.TransferInfo;
 import junit.framework.TestCase;
 
 public class TestMediator extends TestCase {
-	Mediator medMock;
+	Mediator med;
 	UIMediator uiMedMock;
 	Logger mockLogger;
 
@@ -28,26 +31,55 @@ public class TestMediator extends TestCase {
 		mockLogger = mock(Logger.class);
 		Whitebox.setInternalState(Mediator.class, "logger", mockLogger);
 
-		medMock = new Mediator(username, homeDir);
+		med = new Mediator(username, homeDir);
 		uiMedMock = mock(UIMediator.class);
-		medMock.registerUIMediator(uiMedMock);
+		med.registerUIMediator(uiMedMock);
 
 		/* Create mocks for network and networkWorker and set them on mediator. */
 		Network network = mock(Network.class);
 		NetworkWorker networkWorker = mock(NetworkWorker.class);
-		medMock.initMediator(networkWorker, network);
+		med.initMediator(networkWorker, network);
 	}
 
 	/* Assert the path to download a file from local user is build
 	 * using the username's homeDir also.
 	 */
-	public void testBuildOutgoingTransferDir() throws InvocationTargetException, InterruptedException {
+	public void testBuildOutgoingTransferDir() {
 		final ArgumentCaptor<TransferInfo> arg = ArgumentCaptor.forClass(TransferInfo.class);
-		medMock.newOutgoingTransfer("cineva", "path/to/file2");
+		med.newOutgoingTransfer("cineva", "path/to/file2");
 
 		verify(uiMedMock, times(1)).newOutgoingTransfer(arg.capture());
 		assertEquals(arg.getValue().filename, "file2");
 		assertEquals(arg.getValue().path, homeDir + "path/to/");
 	}
 
+	/* Assert Mediator calls UIMediator for rendering concerned logic. */
+	public void testNotifyUIMediatorOnTransferFilesize() throws InvocationTargetException, InterruptedException {
+		final TransferInfo ti = new TransferInfo();
+		ti.id = 1;
+		ti.filesize = 10;
+
+		med.notifyTransferFilesize(ti);
+		SwingUtilities.invokeAndWait(new Runnable() {
+			@Override
+			public void run() {
+				verify(uiMedMock, times(1)).updateTransferFilesize(ti.id, ti.filesize);
+			}
+		});
+	}
+
+	/* Assert Mediator calls UIMediator for rendering concerned logic. */
+	public void testUpdateUIMediatorOnTransferValue() throws InvocationTargetException, InterruptedException {
+		final TransferInfo ti = new TransferInfo();
+		ti.id = 1;
+		final int progress = 50;
+
+		med.updateTransferValue(ti, progress);
+		SwingUtilities.invokeAndWait(new Runnable() {
+			@Override
+			public void run() {
+				verify(uiMedMock, times(1)).updateTransferValue(ti.id, progress);
+			}
+		});
+	}
 }
